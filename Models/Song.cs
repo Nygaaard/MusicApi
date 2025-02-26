7dc6149d619b0707f7fe1b8c5c2d502458f40420
Nygaaard <nygardandreas@hotmail.com>
2025-02-26 09:44:51 +0100

    Added data annotations to model

--- Models/Song.cs
+++ Models/Song.cs
@@ -6,13 +6,17 @@ namespace MusicApi.Models
     {
         //Properties
         public int SongId { get; set; }
-        [Required(ErrorMessage = "Du måste fylla i artist")]
+        [Required(ErrorMessage = "Du måste ange artistens namn.")]
+        [StringLength(100, ErrorMessage = "Artistens namn kan inte vara längre än 100 tecken")]
         public string? Artist { get; set; }
-        [Required(ErrorMessage = "Du måste fylla i titel")]
+        [Required(ErrorMessage = "Du måste ange titel")]
+        [StringLength(100, ErrorMessage = "Titel kan inte vara längre än 100 tecken")]
         public string? Title { get; set; }
-        [Required(ErrorMessage = "Du måste fylla i längd")]
+        [Required(ErrorMessage = "Du måste ange låtens längd")]
+        [Range(1, 1000, ErrorMessage = "Längden måste vara mellan 1 och 1000 sekunder")]
         public int Length { get; set; }
-        [Required(ErrorMessage = "Du måste fylla i kategori")]
+        [Required(ErrorMessage = "Du måste ange kategori")]
+        [StringLength(50, ErrorMessage = "Kategorin kan inte vara längre än 50 tecken")]
         public string? Category { get; set; }
     }
 }
